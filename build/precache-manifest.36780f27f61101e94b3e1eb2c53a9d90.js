6fa6a5dbaf6dd0a0ec431d99f07677bd0b5cf9b3
Damir Zhaksilikov <damika@Damirs-MacBook-Pro.local>
2020-10-19 11:24:45 -0700

    update git ignore

---
+++ build/precache-manifest.36780f27f61101e94b3e1eb2c53a9d90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300f57542c6f09df7d4aba44638415d8",
+    "url": "/doll-house/index.html"
+  },
+  {
+    "revision": "1ad2a3a86d37f23e7796",
+    "url": "/doll-house/static/css/main.7b976c6b.chunk.css"
+  },
+  {
+    "revision": "28b62ba1d43fa7146c73",
+    "url": "/doll-house/static/js/2.6fe3b82d.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/doll-house/static/js/2.6fe3b82d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ad2a3a86d37f23e7796",
+    "url": "/doll-house/static/js/main.687ff358.chunk.js"
+  },
+  {
+    "revision": "1a31f915a938ff0dc704",
+    "url": "/doll-house/static/js/runtime-main.c9571b0f.js"
+  }
+]);
