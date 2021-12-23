3ac63f197bd936229f27ff0a415531c8c271095b
Damir <damir.zhaksilikov@gmail.com>
2021-12-22 16:05:39 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7b63fe4336eeb49c626522015d068c5.js"
+  "/precache-manifest.81bfc49c724aec41c64b30aa6c32a7e8.js"
 );
 
 self.addEventListener('message', (event) => {
