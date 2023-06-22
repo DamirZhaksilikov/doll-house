7b753d75f4836dcfaed8b32ff1babf88039a5ba9
Damir <damir.zhaksilikov@gmail.com>
2023-06-22 17:01:35 +0200

    Offloaded website

--- src/App.tsx
+++ src/App.tsx
@@ -11,53 +11,63 @@ import { RoomData } from './Rooms/_Shared Components/Room/RoomData';
 import { Alias } from './Rooms/_Shared Components/Room/Alias';
 
 export interface AppState {
-    alias: Alias;
+    //    alias: Alias;
 }
 
 export default class App extends React.Component<{}, AppState> {
     constructor(props) {
         super(props);
-        
-        this.state = {
-            alias: new Alias()
-        }
+
+        // this.state = {
+        //     alias: new Alias()
+        // }
     }
 
     public render() {
-        return <div>
-        < BrowserRouter basename={process.env.PUBLIC_URL}>
-            <div>
-                <Route exact path="/">
-                    <Redirect to="/entry" />
-                </Route>
-                <Route path="/entry" render={(props) => (
-                    <Entry {...props} alias={this.state.alias} 
-                    handleAliasUpdate={this.updateAlias.bind(this)} 
-                    room={RoomData.ENTRY} />
-                )} />
-                <Route path="/field_notes" render={(props) => (
-                    <FieldNotes {...props} alias={this.state.alias} room={RoomData.FIELD_NOTES} />
-                )} />
-                <Route path="/mail_room" render={(props) => (
-                    <MailRoom {...props} alias={this.state.alias}  room={RoomData.MAIL_ROOM} />
-                )} />
-                <Route path="/studio" render={(props) => (
-                    <Studio {...props} alias={this.state.alias}  room={RoomData.STUDIO} />
-                )} />
-                <Route path="/rec_room" render={(props) => (
-                    <RecRoom {...props} alias={this.state.alias}  room={RoomData.REC_ROOM} />
-                )} />
-            </div>
-        </ BrowserRouter >
-        <ModalContainer />
+        // return <div>
+        // < BrowserRouter basename={process.env.PUBLIC_URL}>
+        //     <div>
+        //         <Route exact path="/">
+        //             <Redirect to="/entry" />
+        //         </Route>
+        //         <Route path="/entry" render={(props) => (
+        //             <Entry {...props} alias={this.state.alias} 
+        //             handleAliasUpdate={this.updateAlias.bind(this)} 
+        //             room={RoomData.ENTRY} />
+        //         )} />
+        //         <Route path="/field_notes" render={(props) => (
+        //             <FieldNotes {...props} alias={this.state.alias} room={RoomData.FIELD_NOTES} />
+        //         )} />
+        //         <Route path="/mail_room" render={(props) => (
+        //             <MailRoom {...props} alias={this.state.alias}  room={RoomData.MAIL_ROOM} />
+        //         )} />
+        //         <Route path="/studio" render={(props) => (
+        //             <Studio {...props} alias={this.state.alias}  room={RoomData.STUDIO} />
+        //         )} />
+        //         <Route path="/rec_room" render={(props) => (
+        //             <RecRoom {...props} alias={this.state.alias}  room={RoomData.REC_ROOM} />
+        //         )} />
+        //     </div>
+        // </ BrowserRouter >
+        // <ModalContainer />
+        // </div>
+
+        return <div id="closing-notice">
+            {"Shut for the time being."}
+            <br></br><br></br>
+
+            {"If you have my number I probably miss you, so give me a call or shoot me a text."}
+            <br></br>
+            <a href="mailto:damir.zhaksilikov@gmail.com">otherwise, you can always reach me by mail.</a>
+            
         </div>
     }
 
     public updateAlias(value: string) {
         this.setState({
-           alias: new Alias(value) 
+            //   alias: new Alias(value) 
         });
     }
 }
 
-   
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppOrientationWrapper } from './AppOrientationWrapper';
+import App from './App';
 
-ReactDOM.render(<AppOrientationWrapper />, document.getElementById("root"));
+// ReactDOM.render(<AppOrientationWrapper />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
